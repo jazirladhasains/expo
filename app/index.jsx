ef650322b7f3f52b759ed2470a0ff694bb8fb87b
Jazir Ladha <jazir.ladha@sainsburys.co.uk>
2024-09-03 21:52:16 +0100

    splashscreen complete, all working

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,23 @@
 
 import { Image, ScrollView, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from '../constants'
+import CustomButton from "../components/CustomButton";
+import { Redirect, router } from "expo-router";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image source={images.logo} 
           className="w-[130px] h-[84px]" 
           resizeMode="contain"/>
           <Image source={images.cards}
           className="max-w-[380px] w-full h-[300px]"
           resizeMode="contain"/>
-        </View>
-        <View className="relative mt-5">
+          <View className="relative mt-5">
           <Text className="text-3xl text-white font-bold text-center">
             Discover endless Possibilities with {' '}
             <Text className="text-secondary-200">Aora</Text>
@@ -24,8 +26,18 @@ export default function App() {
           source={images.path}
           className="w-[136px] h-[15px] absolute -bottom-2 -right-8" 
           resizeMode="contain"/>
+          </View>
+          <Text className="text-sm font-preregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a limitless exploration with Aora
+          </Text>
+          <CustomButton  
+          title="Continue with Email"
+          handlePress={() => {router.push('./sign-in')}}
+          containerStyles="w-full mt-7"/>
         </View>
+        
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light'/>
     </SafeAreaView>
   );
 }
