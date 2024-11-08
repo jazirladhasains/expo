0b189846618d210d83e3c2f34274fc431f98e5ca
Jazir Ladha <jazir.ladha@sainsburys.co.uk>
2024-11-08 15:39:32 +0000

    profile screen complete

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -6,9 +6,11 @@ import FormField from '../../components/FormField'
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     username: '',
     email: '',
